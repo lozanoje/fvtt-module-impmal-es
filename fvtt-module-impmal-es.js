c98513fc002d24e9362d557d7d27dc6eb3656bc9
José E. Lozano <lozanoje@gmail.com>
2025-09-26 21:19:52 +0200

    v3.2.0

--- fvtt-module-impmal-es.js
+++ fvtt-module-impmal-es.js
@@ -1,6 +1,6 @@
 Hooks.once("ready", async function () {
-	console.log("language:" + game.settings.get("core", "language"))
-    if (game.user.isGM && game.settings.get("core", "language") === "es") {
-	  document.getElementById("logo").src="/modules/fvtt-module-impmal-es/imagenes/fvtt-impmal-es.webp";
-  }
+  // 	console.log("language:" + game.settings.get("core", "language"))
+  //     if (game.user.isGM && game.settings.get("core", "language") === "es") {
+  // 	  document.getElementById("logo").src="/modules/fvtt-module-impmal-es/imagenes/fvtt-impmal-es.webp";
+  //   }
 });
